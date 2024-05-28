f9c29d5bb0e37f78e072c109fa9fcaaa1f235387
hagd <hagd0520@gmail.com>
2024-05-28 14:07:44 +0900

    10-10

--- routers/auth.py
+++ routers/auth.py
@@ -1,10 +1,18 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from passlib.context import CryptContext
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from starlette import status
 
+from database import SessionLocal
 from models import Users
 
 
 router = APIRouter()
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -13,17 +21,35 @@ class CreateUserRequest(BaseModel):
     last_name: str
     password: str
     role: str
+    
+    
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+        
+db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@router.post("/auth")
-async def create_user(create_user_request: CreateUserRequest):
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(
+    db: db_dependency,
+    create_user_request: CreateUserRequest
+):
     create_user_model = Users(
         email=create_user_request.email,
         username=create_user_request.username,
         first_name=create_user_request.first_name,
         last_name=create_user_request.last_name,
         role=create_user_request.role,
-        hashed_password=create_user_request.password,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
         is_active=True
     )
-    return {"user": "authenticated"}
+    
+    db.add(create_user_model)
+    db.commit()
+    db.refresh(create_user_model)
+    return {"user": create_user_model}
