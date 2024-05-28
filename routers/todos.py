c64de8832983145dad25960cfcfc8456cac487d1
hagd <hagd0520@gmail.com>
2024-05-28 16:12:50 +0900

    11-1 Auth on Post Todo

--- routers/todos.py
+++ routers/todos.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 from starlette import status
 from models import Todos
 from database import Base, SessionLocal, engine
-from routers import auth
+from routers.auth import get_current_user
 
 
 router = APIRouter()
@@ -20,6 +20,7 @@ def get_db():
         
         
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
     
     
 class TodoRequest(BaseModel):
@@ -36,7 +37,10 @@ async def read_all(db: db_dependency):
 
 
 @router.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
-async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+async def read_todo( 
+    db: db_dependency, 
+    todo_id: int = Path(gt=0)
+):
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
     if todo_model:
         return todo_model
@@ -44,8 +48,15 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
 
 
 @router.post("/todo", status_code=status.HTTP_201_CREATED)
-async def create_todo(db: db_dependency, todo_request: TodoRequest):
-    todo_model = Todos(**todo_request.model_dump())
+async def create_todo(
+    user: user_dependency,
+    db: db_dependency, 
+    todo_request: TodoRequest
+):
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed")
+
+    todo_model = Todos(**todo_request.model_dump(), owner_id=user["id"])
     
     db.add(todo_model)
     db.commit()
