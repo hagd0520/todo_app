caf193601c254c0f658f43c7c81736f64648c274
hagd <hagd0520@gmail.com>
2024-05-28 20:28:27 +0900

    12 init for postgres

--- database.py
+++ database.py
@@ -6,7 +6,8 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
