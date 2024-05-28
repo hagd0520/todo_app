9e0b232faaa59f0ede3b0f054e6dcf0224e82f3f
hagd <hagd0520@gmail.com>
2024-05-28 19:11:35 +0900

    11-8 Users Route

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 import models
 from database import Base, engine
-from routers import auth, todos, admin
+from routers import auth, todos, admin, users
 
 
 app = FastAPI()
@@ -12,3 +12,4 @@ Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
+app.include_router(users.router)
