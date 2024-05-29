60f19f055792733e8aa8439d00530ad1c3acf3bb
hagd <hagd0520@gmail.com>
2024-05-29 15:30:29 +0900

    14-16 Test Part 4 utils

--- test/test_todos.py
+++ test/test_todos.py
@@ -1,64 +1,14 @@
-import pytest
-from sqlalchemy import StaticPool, create_engine, text
-from sqlalchemy.orm import sessionmaker
 from starlette import status
-from starlette.testclient import TestClient
 
-from database import Base
 from main import app
 from models import Todos
 from routers.todos import get_db, get_current_user
 
-
-DATABASE_URL = "sqlite:///./testdb.db"
-
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False},
-    poolclass= StaticPool
-)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-Base.metadata.create_all(bind=engine)
-
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-        
-        
-def override_get_current_user():
-    return {"username": "test", "id": 1, "user_role": "admin"}
+from utils import *
         
 
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
-
-
-client = TestClient(app)
-
-
-@pytest.fixture
-def test_todo():
-    todo = Todos(
-        title="Learn to code!",
-        description="Need to learn everyday!",
-        priority=5,
-        complete=False,
-        owner_id=1
-    )
-    
-    db = TestingSessionLocal()
-    db.add(todo)
-    db.commit()
-    yield todo
-    with engine.connect() as conn:
-        conn.execute(text("DELETE FROM todos;"))
-        conn.commit()
 
 
 def test_read_all_authenticated(test_todo):
---
+++ test/utils.py
@@ -0,0 +1,55 @@
+import pytest
+from sqlalchemy import StaticPool, create_engine, text
+from sqlalchemy.orm import sessionmaker
+from starlette.testclient import TestClient
+from database import Base
+
+from main import app
+from models import Todos
+
+
+DATABASE_URL = "sqlite:///./testdb.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass= StaticPool
+)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base.metadata.create_all(bind=engine)
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+        
+def override_get_current_user():
+    return {"username": "test", "id": 1, "user_role": "admin"}
+
+
+client = TestClient(app)
+
+
+@pytest.fixture
+def test_todo():
+    todo = Todos(
+        title="Learn to code!",
+        description="Need to learn everyday!",
+        priority=5,
+        complete=False,
+        owner_id=1
+    )
+    
+    db = TestingSessionLocal()
+    db.add(todo)
+    db.commit()
+    yield todo
+    with engine.connect() as conn:
+        conn.execute(text("DELETE FROM todos;"))
+        conn.commit()
