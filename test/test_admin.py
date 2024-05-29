4a15b2bc90d2c967c033e79bec2dddbb7e146996
hagd <hagd0520@gmail.com>
2024-05-29 16:06:54 +0900

    14-18 Test Part 6

--- test/test_admin.py
+++ test/test_admin.py
@@ -6,6 +6,7 @@ from test.utils import *
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
 
+
 def test_admin_read_all_authenticated(test_todo):
     response = client.get("/admin/todo")
     assert response.status_code == status.HTTP_200_OK
@@ -17,3 +18,18 @@ def test_admin_read_all_authenticated(test_todo):
         "priority": 5,
         "owner_id": 1
     }]
+    
+    
+def test_admin_delete_todo(test_todo):
+    response = client.delete("/admin/todo/1")
+    assert response.status_code == 204
+    
+    db = TestingSessionLocal()
+    model = db.query(Todos).where(Todos.id == 1).first()
+    assert not model
+    
+    
+def test_admin_delete_todo_not_found():
+    response = client.delete("/admin/todo/999")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Todo not found."}
