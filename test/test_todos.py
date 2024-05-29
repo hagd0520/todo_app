415c257bc29bb8fdbb3fa05db278990442623186
hagd <hagd0520@gmail.com>
2024-05-29 14:46:00 +0900

    14-10 Setup Dependencies

---
+++ test/test_todos.py
@@ -0,0 +1,46 @@
+from sqlalchemy import StaticPool, create_engine
+from sqlalchemy.orm import sessionmaker
+from starlette import status
+from starlette.testclient import TestClient
+
+from database import Base
+from main import app
+from routers.todos import get_db, get_current_user
+
+
+DATABASE_URL = "sqlite:///./testdb.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass= StaticPool
+)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base.metadata.create_all(bind=engine)
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+        
+def override_get_current_user():
+    return {"username": "test", "id": 1, "user_role": "admin"}
+        
+
+app.dependency_overrides[get_db] = override_get_db
+app.dependency_overrides[get_current_user] = override_get_current_user
+
+
+client = TestClient(app)
+
+
+def test_read_all_authenticated():
+    response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == []
