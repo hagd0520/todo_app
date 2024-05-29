3b6e67ebf898be48b40a74b305cd35f2dcb77892
hagd <hagd0520@gmail.com>
2024-05-29 15:14:03 +0900

    14-14 Test Part 2

--- test/test_todos.py
+++ test/test_todos.py
@@ -71,3 +71,41 @@ def test_read_all_authenticated(test_todo):
         "id": 1, "priority": 5,
         "owner_id": 1
     }]
+    
+    
+def test_read_one_authenticated(test_todo):
+    response = client.get("/todo/1")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        "complete":False, 
+        "title": "Learn to code!", 
+        "description": "Need to learn everyday!", 
+        "id": 1, "priority": 5,
+        "owner_id": 1
+    }
+    
+    
+def test_read_one_authenticated_not_found():
+    response = client.get("/todo/999")
+    assert response.status_code == 404
+    assert response.json() == {'detail': "Todo not found."}
+    
+    
+def test_create_todo(test_todo):
+    request_data={
+        "title": "New Todo!",
+        "description": "New todo description",
+        "priority": 5,
+        "complete": False
+    }
+    
+    
+    response = client.post("/todo", json=request_data)
+    assert response.status_code == 201
+    
+    db = TestingSessionLocal()
+    model = db.query(Todos).where(Todos.id == 2).first()
+    assert model.title == request_data.get("title")
+    assert model.description == request_data.get("description")
+    assert model.priority == request_data.get("priority")
+    assert model.complete == request_data.get("complete")
