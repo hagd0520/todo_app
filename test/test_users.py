09343a83152978869a841e247ff9890590ce1b6e
hagd <hagd0520@gmail.com>
2024-05-29 16:27:32 +0900

    14-20 Test Part 8

--- test/test_users.py
+++ test/test_users.py
@@ -17,3 +17,18 @@ def test_return_user(test_user):
     assert response.json()["role"] == "admin"
     assert response.json()["phone_number"] == "(111)-111-1111"
     
+    
+def test_change_password_success(test_user):
+    response = client.put("/user/password", json={"password": "1234", "new_password": "newpassword"})
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    
+    
+def test_change_password_invalid_current_password(test_user):
+    response = client.put("/user/password", json={"password": "wrong_password", "new_password": "newpassword"})
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == {"detail": "Error on password change"}
+    
+    
+def test_change_phone_number_success(test_user):
+    response = client.put("/user/phone-number/222222222")
+    assert response.status_code == status.HTTP_204_NO_CONTENT
